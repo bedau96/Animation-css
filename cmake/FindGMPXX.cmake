2c8ea5d014117aee1c68379e6493b33959aed7eb
bedau96 <92643251+bedau96@users.noreply.github.com>
2020-02-13 22:42:46 +0000

    menu fixed

--- cmake/FindGMPXX.cmake
+++ cmake/FindGMPXX.cmake
@@ -7,4 +7,19 @@ find_library(GMPXX_LIBRARY NAMES gmpxx)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GMPXX
-    FOUND_VAR G
+    FOUND_VAR GMPXX_FOUND
+    REQUIRED_VARS
+        GMPXX_LIBRARY
+        GMPXX_INCLUDE_DIR
+)
+
+if(GMPXX_FOUND AND NOT TARGET GMP::GMPXX)
+    add_library(GMP::GMPXX UNKNOWN IMPORTED)
+    set_target_properties(GMP::GMPXX PROPERTIES
+        IMPORTED_LOCATION "${GMPXX_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${GMPXX_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(
+    

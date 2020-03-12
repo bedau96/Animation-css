72e949ba5a2089dd0ee6a557a904a771eb8974cd
bedau96 <92643251+bedau96@users.noreply.github.com>
2020-03-12 07:32:03 +0000

    Format nit.

---
+++ src/ENCRYPTO_utils/cbitvector.cpp
@@ -0,0 +1,6 @@
+/**
+ \file 		cbitvector.cpp
+ \author 	michael.zohner@ec-spride.de
+ \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
+			Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+			This program is free software: you can redistr

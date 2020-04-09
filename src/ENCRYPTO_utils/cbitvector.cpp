a7a02d486a10ea78304ff3e812f7d85319f38033
bedau96 <92643251+bedau96@users.noreply.github.com>
2020-04-09 05:36:13 +0000

    0.0.57

--- src/ENCRYPTO_utils/cbitvector.cpp
+++ src/ENCRYPTO_utils/cbitvector.cpp
@@ -178,4 +178,22 @@ void CBitVector::delCBitVector() {
 void CBitVector::FillRand(std::size_t bits, crypto* crypt) {
 	if (bits > m_nByteSize << 3)
 		Create(bits);
-	crypt->gen_rnd(m_pBits, cei
+	crypt->gen_rnd(m_pBits, ceil_divide(bits, 8));
+}
+
+void CBitVector::CreateExact(std::size_t bits) {
+	if (bits == 0){
+		bits = AES_BITS;
+	}
+
+	// if memory was previously allocated: free it
+	if (m_nByteSize > 0) {
+		free(m_pBits);
+	}
+
+	m_nByteSize = ceil_divide(bits, 8);
+	m_pBits = (BYTE*) calloc(m_nByteSize, sizeof(BYTE));
+	assert(m_pBits != NULL);
+
+	m_nElementLength = 1;
+	m_nNumElements = m_nByte

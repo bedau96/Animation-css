3f140fda4975d0b9505fc03986b4c2cb029000cd
bedau96 <92643251+bedau96@users.noreply.github.com>
2020-10-11 16:58:19 +0000

    Fix spelling mistakes.

--- src/ENCRYPTO_utils/cbitvector.h
+++ src/ENCRYPTO_utils/cbitvector.h
@@ -1,4 +1,9 @@
 /**
  \file 		cbitvector.h
  \author 	michael.zohner@ec-spride.de
- \copyright	ABY - A Framework for Efficient 
+ \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
+			Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+			This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published
+            by the Free Software Foundation, either version 3 of the License, or
+            (

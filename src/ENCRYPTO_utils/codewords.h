6d2edb1e523f2c1bb01d1317d133dba9b912b6aa
bedau96 <92643251+bedau96@users.noreply.github.com>
2021-07-20 18:53:38 +0000

    Release 8.1.0

--- src/ENCRYPTO_utils/codewords.h
+++ src/ENCRYPTO_utils/codewords.h
@@ -9,4 +9,8 @@
             (at your option) any later version.
             ABY is distributed in the hope that it will be useful,
             but WITHOUT ANY WARRANTY; without even the implied warranty of
-            MERCHANTABILITY or FITNE
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+            GNU Lesser General Public License for more details.
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		A hard-coded Welsh-Hadamard code for up to 8-bit words and resulting 256-bit codew

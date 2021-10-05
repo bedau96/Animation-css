ad488e9ddafa7ebbc5c8734b36f7fe3ad986723f
bedau96 <92643251+bedau96@users.noreply.github.com>
2021-10-05 22:07:21 +0000

    Add performance tests

---
+++ src/ENCRYPTO_utils/crypto/Config.h
@@ -0,0 +1,5 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project

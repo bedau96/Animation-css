c3816b7cb5da9ea6af497300a086e9bf9ec77a6c
bedau96 <92643251+bedau96@users.noreply.github.com>
2021-10-06 20:30:36 +0000

    style changes.

--- src/ENCRYPTO_utils/crypto/Config.h
+++ src/ENCRYPTO_utils/crypto/Config.h
@@ -2,4 +2,9 @@
 * %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 * 
 * Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
-* This file is part of the SCAPI project
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or

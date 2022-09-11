461f786ecf17d3d7b1326ed170347d29e5597bc0
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-09-11 12:21:22 +0000

    v1.3

--- src/ENCRYPTO_utils/crypto/crypto.cpp
+++ src/ENCRYPTO_utils/crypto/crypto.cpp
@@ -291,4 +291,11 @@ void crypto::fixed_key_aes_hash(AES_KEY_CTX* aes_key, uint8_t* resbuf, uint32_t 
 	memset(aes_hash_in_buf, 0, AES_BYTES);
 	memcpy(aes_hash_in_buf, inbuf, ninbytes);
 
-	//two encryption iterations TODO: not secure since bot
+	//two encryption iterations TODO: not secure since both blocks are treated independently, implement DM or MMO
+#ifdef OPENSSL_OPAQUE_EVP_CIPHER_CTX
+	EVP_EncryptUpdate(*aes_key, aes_hash_out_buf, &dummy, aes_hash_in_buf, AES_BYTES);
+#else
+	EVP_EncryptUpdate(aes_key, aes_hash_out_buf, &dummy, aes_hash_in_buf, AES_BYTES);
+#endif
+
+	((uint64_t*) aes_hash_out_buf)[0] ^= ((uint64_t*) aes_h

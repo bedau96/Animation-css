063ce4fa6fc0cb63b6c83841bc207d039d76edff
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-09-25 07:49:38 +0000

    Grammar fix.

--- src/ENCRYPTO_utils/crypto/crypto.cpp
+++ src/ENCRYPTO_utils/crypto/crypto.cpp
@@ -353,4 +353,16 @@ uint32_t crypto::get_hash_bytes() {
 //Generate a common seed, is only secure in the semi-honest model
 void crypto::gen_common_seed(prf_state_ctx* prf_state, CSocket& sock) {
 	uint8_t *seed_buf, *seed_rcv_buf;
-	uint
+	uint32_t seed_bytes, i;
+
+	seed_bytes = get_aes_key_bytes();
+	seed_buf = (uint8_t*) malloc(seed_bytes);
+	seed_rcv_buf = (uint8_t*) malloc(seed_bytes);
+
+	//randomly generate and exchange seed bytes:
+	gen_rnd(seed_buf, seed_bytes);
+	sock.Send(seed_buf, seed_bytes);
+	sock.Receive(seed_rcv_buf, seed_bytes);
+
+	//xor both seeds
+	for (i = 0; i < seed_bytes; 

32a8932f9cf86dcb2a4de1794aad1c89791297d9
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-10-11 21:17:35 +0000

    Changed

--- src/ENCRYPTO_utils/crypto/crypto.h
+++ src/ENCRYPTO_utils/crypto/crypto.h
@@ -9,4 +9,17 @@
 			(at your option) any later version.
 			ABY is distributed in the hope that it will be useful,
 			but WITHOUT ANY WARRANTY; without even the implied warranty of
-			MERCHANTABILIT
+			MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+			GNU Lesser General Public License for more details.
+			You should have received a copy of the GNU Lesser General Public License
+			along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		Crypto primitive class
+ */
+
+#ifndef CRYPTO_H_
+#define CRYPTO_H_
+
+#include <openssl/evp.h>
+#include "../constants.h"
+#include <mutex>
+

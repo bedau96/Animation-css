982db0fb5d1b8c124a9ec80302e879d61d211ead
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-12-09 07:20:06 +0000

    msm: mdss: Remove deprecated __dev* markings

--- src/ENCRYPTO_utils/graycode.cpp
+++ src/ENCRYPTO_utils/graycode.cpp
@@ -2,4 +2,10 @@
  \file 		graycode.cpp
  \author 	Martin Kromm<martin.kromm@stud.tu-darmstadt.de>
  \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
-			Copyright (C) 2019 ENCRYPTO Group, TU Darms
+			Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+			This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published
+            by the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+            ABY is distributed in the hope that it will be useful,
+            bu

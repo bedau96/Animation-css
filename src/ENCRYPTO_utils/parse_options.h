8ccbe96b645caa2a8b2a1c4d278b29a3f05ddf03
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-12-16 17:36:19 +0000

    Update badge.

--- src/ENCRYPTO_utils/parse_options.h
+++ src/ENCRYPTO_utils/parse_options.h
@@ -2,4 +2,9 @@
  \file 		parse_options.h
  \author 	michael.zohner@ec-spride.de
  \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
-			Copyright (
+			Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+			This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published
+            by the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+   

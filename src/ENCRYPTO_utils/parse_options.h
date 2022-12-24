49a99a4237d9577726ea98e4ac97c9056556e3a1
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-12-24 14:54:33 +0000

    Update build tools version

--- src/ENCRYPTO_utils/parse_options.h
+++ src/ENCRYPTO_utils/parse_options.h
@@ -45,4 +45,12 @@ struct parsing_ctx {
 	std::string opt_name; //name to set the parameter via command line
 	std::string help_str; //definition of the parameter that is printed in print_usage
 	bool required; //is the parameter required to run the program? If required and not set by the invocation, program will exit
-	bool set; //has the value for the parameter been set previously? In case the parameter is read, 
+	bool set; //has the value for the parameter been set previously? In case the parameter is read, this will be set to true
+};
+
+
+/**
+	This method parses the command line arguments from a C program, given in argcp and argcv, using the flags and parameters specified
+	in options where nops gives the number of parameters that are parsed. The values for the parameters are written into options.
+
+	\param  argcp	 - Pointer to

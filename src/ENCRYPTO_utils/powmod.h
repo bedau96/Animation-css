2e3a997232e8c0a28c058b5d4bdaa104a90fe9d2
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-12-25 14:58:00 +0000

    Removed debugging statement

--- src/ENCRYPTO_utils/powmod.h
+++ src/ENCRYPTO_utils/powmod.h
@@ -1,1 +1,8 @@
 /**
+ \file 		powmod.h
+ \author 	daniel.demmler@ec-spride.de
+ \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
+			Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+			This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published
+            by the Free Softw

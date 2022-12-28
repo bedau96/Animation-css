bb86f594c4f4653fda868ced3fa007250290aef6
bedau96 <92643251+bedau96@users.noreply.github.com>
2022-12-28 17:03:24 +0000

    Fetched translations and updated catalogs.

--- src/ENCRYPTO_utils/powmod.h
+++ src/ENCRYPTO_utils/powmod.h
@@ -10,4 +10,19 @@
             ABY is distributed in the hope that it will be useful,
             but WITHOUT ANY WARRANTY; without even the implied warranty of
             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-            GNU Lesser General Public License for more details
+            GNU Lesser General Public License for more details.
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		Powmod Implementation
+ */
+
+#ifndef _POWMOD_H_
+#define _POWMOD_H_
+
+#include <gmp.h>
+
+extern mpz_t* m_table_g;
+extern mpz_t* m_table_h;
+extern mpz_t* m_prod;
+extern mpz_t m_mod;
+extern size_t m_numberOfElem

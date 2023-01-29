b52deb50892cbc8550860d9c7195533ff1f65667
bedau96 <92643251+bedau96@users.noreply.github.com>
2023-01-29 10:55:53 +0000

    removed binary files

--- src/ENCRYPTO_utils/sndthread.cpp
+++ src/ENCRYPTO_utils/sndthread.cpp
@@ -11,4 +11,19 @@
             but WITHOUT ANY WARRANTY; without even the implied warranty of
             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
             GNU Lesser General Public License for more details.
-            You should have received a copy of t
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		Receiver Thread Implementation
+ */
+
+
+#include "sndthread.h"
+#include "socket.h"
+#include "constants.h"
+#include <cassert>
+#include <cstring>
+
+
+SndThread::SndThread(CSocket* sock, CLock *glock)
+: mysock(sock), sndlock(glock), send(std::make_unique<CEvent>())
+{

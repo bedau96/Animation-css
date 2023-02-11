a28a08f9bc8f427bdad7353f57865dc382a07c7d
bedau96 <92643251+bedau96@users.noreply.github.com>
2023-02-11 13:58:30 +0000

    Stats archive

--- src/ENCRYPTO_utils/socket.h
+++ src/ENCRYPTO_utils/socket.h
@@ -1,4 +1,10 @@
 /**
  \file 		socket.h
  \author 	Seung Geol Choi
- \copyright	ABY - A Fr
+ \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
+ Copyright (C) 2019 ENCRYPTO Group, TU Darmstadt
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ABY

b03375830674885be1c3db1d49eca3b732204bec
bedau96 <92643251+bedau96@users.noreply.github.com>
2023-03-27 14:36:06 +0000

    Improve readme file

--- src/ENCRYPTO_utils/utils.h
+++ src/ENCRYPTO_utils/utils.h
@@ -8,4 +8,12 @@
             by the Free Software Foundation, either version 3 of the License, or
             (at your option) any later version.
             ABY is distributed in the hope that it will be useful,
-            but WITHOUT ANY WA
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+            GNU Lesser General Public License for more details.
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		utils
+ */
+
+#ifndef _UTILS_H_
